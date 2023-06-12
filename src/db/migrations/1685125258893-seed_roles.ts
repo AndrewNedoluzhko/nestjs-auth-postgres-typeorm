6603e260375d2ac01d25460744def18af303c3f1
Andrii Nedoluzhko <andrii.nedoluzhko@gmail.com>
2023-06-12 13:01:18 +0300

    updated ormconfig and seed_roles

--- src/db/migrations/1685125258893-seed_roles.ts
+++ src/db/migrations/1685125258893-seed_roles.ts
@@ -1,29 +1,26 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
-//import { Role } from "src/roles/entities/role.entity";
 import { Role } from "../../roles/entities/role.entity";
-
 export class SeedRoles1565812987671 implements MigrationInterface {
-    //timestamp = Date.now();
-    name = `SeedRoles1565812987671`;
+  name = `SeedRoles1565812987671`;
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
+  public async up(queryRunner: QueryRunner): Promise<any> {
 
-      const rolesRepo = queryRunner.manager.getRepository(Role);
+    const rolesRepo = queryRunner.manager.getRepository(Role);
 
-      const  userRole= rolesRepo.create({
-        id: 1,
-        name: 'User'
-      });
+    const userRole = rolesRepo.create({
+      id: 1,
+      name: 'User'
+    });
 
-      const  adminRole= rolesRepo.create({
-        id: 2,
-        name: 'Admin'
-      });
-      await rolesRepo.save([userRole, adminRole]);
-    }
+    const adminRole = rolesRepo.create({
+      id: 2,
+      name: 'Admin'
+    });
+    await rolesRepo.save([userRole, adminRole]);
+  }
 
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    public async down(queryRunner: QueryRunner): Promise<any> {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public async down(queryRunner: QueryRunner): Promise<any> { }
 }
 
 
--- src/db/ormconfig.ts
+++ src/db/ormconfig.ts
@@ -1,18 +1,17 @@
 
 import { ConfigService } from "@nestjs/config";
-import {config} from "dotenv";
+import { config } from "dotenv";
 //import { User } from "src/users/entities/user.entity";
 import { User } from "../users/entities/user.entity";
+import { Role } from "../roles/entities/role.entity";
 import { DataSource, DataSourceOptions } from "typeorm";
 import { CreatedMigration1685382494385 } from "./migrations/1685382494385-created_migration";
 import { GeneratedMigration1685382819168 } from "./migrations/1685382819168-generated_migration";
 import { SeedRoles1565812987671 } from "./migrations/1685125258893-seed_roles";
-import { Role } from "../roles/entities/role.entity";
 import { GeneratedMigration1685795112108 } from "./migrations/1685795112108-generated_migration";
 
 config();
 const configService = new ConfigService();
-
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host: configService.get('DB_HOST'),
@@ -24,21 +23,20 @@ export const dataSourceOptions: DataSourceOptions = {
   synchronize: true,
   migrationsTableName: 'migrations',
   entities: [
-      User, Role
-      //'dist/**/entities/*.entity{.ts,.js}'
+    User, Role
+    //'dist/**/entities/*.entity{.ts,.js}'
   ],
   migrations: [
     CreatedMigration1685382494385,
     GeneratedMigration1685382819168,
     SeedRoles1565812987671,
     GeneratedMigration1685795112108
-      //'dist/db/migrations/*.js'   
-  ]   
+    //'dist/db/migrations/*.js'   
+  ]
 }
 
 export const dataSource = new DataSource(dataSourceOptions);
-
 dataSource.initialize()
-  .then(()=> console.log("Data Source has been initialized"))
-  .catch((error)=>console.log("Error initializating Data Source", error))
+  .then(() => console.log("Data Source has been initialized"))
+  .catch((error) => console.log("Error initializating Data Source", error))
 
